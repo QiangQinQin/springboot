4ac320a5bdee68825aec1401ed290f60346f92bb
QiangQinQin <2901422401@qq.com>
2022-11-19 11:35:42 +0800

    统一springboot版本+编写redisTemplate使用代码+测试日志打印

--- src/test/java/com/tulun/redisTest.java
+++ src/test/java/com/tulun/redisTest.java
@@ -2,15 +2,14 @@ package com.tulun;
 
 import static org.junit.Assert.assertTrue;
 
-import org.assertj.core.api.Assertions;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
-import org.springframework.stereotype.Service;
-import org.springframework.test.context.ContextConfiguration;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.annotation.Resource;
@@ -18,27 +17,43 @@ import javax.annotation.Resource;
 /**
  *
  */
-//todo:@value  @Bean测试resdis
+//todo:@value  @Bean测试resdis  @Autowired 和 @Resource
 //@Service
 @RunWith(SpringRunner.class)
 //@ContextConfiguration(locations = {"classpath:Application.yml"})
 @SpringBootTest(classes = HelloApplication.class)
+@Slf4j
 public class redisTest
 {
-//    @Resource  //https://blog.csdn.net/cheers_bin/article/details/125637817   https://www.cnblogs.com/wangrong1/p/12187674.html
-//    private RedisTemplate<String,Object> redisTemplate;// <1>
-//
+    //https://blog.csdn.net/cheers_bin/article/details/125637817   https://www.cnblogs.com/wangrong1/p/12187674.html
 //    @Resource
-//    private StringRedisTemplate StringRedisTemplate;
-//
+//    private RedisTemplate<String,Object> redisTemplate;// <1>
+
     @Autowired
-    private RedisTemplate redisTemplate;
+    private StringRedisTemplate stringRedisTemplate;
+
+//    @Resource
+//    private RedisTemplate RedisTemplate;
 //
+
     @Test
     public void test() throws Exception {
-        System.out.println("hello");
-        redisTemplate.opsForValue().set("student:1", "kirito"); // <2>
-        System.out.println( redisTemplate.opsForValue().get("student:1"));
-//        Assertions.assertThat(redisTemplate.opsForValue().get("student:1")).isEqualTo("kirito");
+        System.out.println("hello ");
+        if(stringRedisTemplate.hasKey("test")){
+            stringRedisTemplate.opsForValue().increment("test",1L); //ERR value is not an integer or out of range
+        }else{
+            stringRedisTemplate.setKeySerializer(new StringRedisSerializer());
+            stringRedisTemplate.setValueSerializer(new StringRedisSerializer());
+            stringRedisTemplate.opsForValue().set("test", "1");
+        }
+        System.out.println( stringRedisTemplate.opsForValue().get("test"));
+    }
+
+    @Test
+    public void testLog() throws Exception {
+        String out = "test1111";
+        log.info("idass:{}", out);
+        log.error("idass:{}", out);
+        log.warn("idass:{}", out);
     }
 }
