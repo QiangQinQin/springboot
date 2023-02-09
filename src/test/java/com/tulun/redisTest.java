bdac5fba21a7135aada10b86c192b30e5f2a9b66
QiangQinQin <2901422401@qq.com>
2023-02-10 00:31:15 +0800

    测试HttpClient

--- src/test/java/com/tulun/redisTest.java
+++ src/test/java/com/tulun/redisTest.java
@@ -3,6 +3,12 @@ package com.tulun;
 import static org.junit.Assert.assertTrue;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +19,7 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.annotation.Resource;
+import java.io.IOException;
 
 /**
  *
@@ -47,6 +54,28 @@ public class redisTest
             stringRedisTemplate.opsForValue().set("test", "1");
         }
         System.out.println( stringRedisTemplate.opsForValue().get("test"));
+    }
+
+
+//    教程： https://www.cnblogs.com/kitor/p/11235762.html
+    @Test
+    public void testHttpClient() throws IOException {
+        //1.打开浏览器
+        CloseableHttpClient httpClient = HttpClients.createDefault();
+        //2.声明get请求
+        HttpGet httpGet = new HttpGet("http://www.baidu.com/s?wd=java");
+        //3.发送请求
+        CloseableHttpResponse response = httpClient.execute(httpGet);
+        //4.判断状态码
+        if(response.getStatusLine().getStatusCode()==200){
+            HttpEntity entity = response.getEntity();
+            //使用工具类EntityUtils，从响应中取出实体表示的内容并转换成字符串
+            String string = EntityUtils.toString(entity, "utf-8");
+            System.out.println(string);
+        }
+        //5.关闭资源
+        response.close();
+        httpClient.close();
     }
 
 }
